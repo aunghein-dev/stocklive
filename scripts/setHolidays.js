0e186524bf298301ea625776599657f9e3679e03
Aung Hein <aunghein.mm@hotmail.com>
2025-04-11 13:55:43 +0630

    bug fixes

--- scripts/setHolidays.js
+++ scripts/setHolidays.js
@@ -56,28 +56,35 @@ async function renderSetHolidays() {
 
 
 function generateHolidaysDetailsHTML(month) {
+  const currentYear = dayjs().format("YYYY");
+
+  // Filter and sort holidays for the given month
+  const filteredHolidays = holidays
+    .filter(holiday =>
+      dayjs(holiday.date).format("MMMM") === month &&
+      dayjs(holiday.date).format("YYYY") === currentYear
+    )
+    .sort((a, b) => new Date(a.date) - new Date(b.date)); // Sort by date ascending
+
   let innerHTML = "";
 
-  holidays.reverse().forEach((holiday) => {
-    if (dayjs(holiday.date).format("MMMM") === month && 
-    dayjs(holiday.date).format("YYYY") === dayjs().format("YYYY")
-    ) {
-      innerHTML += `
-        <div class="right-details-row-box">
-          <div class="inner-left-box">
-            <div class="holiday-date">${dayjs(holiday.date).format("D")}</div>
-          </div>
-          <div class="inner-right-box">
-            <div class="inner-right-row1">${holiday.name}</div>
-            <div class="inner-right-row2">${dayjs(holiday.date).format("dddd")}</div>
-          </div>
+  filteredHolidays.forEach((holiday) => {
+    innerHTML += `
+      <div class="right-details-row-box">
+        <div class="inner-left-box">
+          <div class="holiday-date">${dayjs(holiday.date).format("D")}</div>
         </div>
-      `;
-    } 
+        <div class="inner-right-box">
+          <div class="inner-right-row1">${holiday.name}</div>
+          <div class="inner-right-row2">${dayjs(holiday.date).format("dddd")}</div>
+        </div>
+      </div>
+    `;
   });
 
   return innerHTML;
 }
+
 
 
  function addingLoadingPage() {
